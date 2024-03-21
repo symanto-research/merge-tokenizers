9810e7366866285f435b14dee8efe6b8733d2f05
José Ángel González <jose.gonzalez@symanto.com>
2024-03-21 17:03:02 +0100

    remove requirements

--- setup.py
+++ setup.py
@@ -5,12 +5,28 @@ VERSION: Dict[str, str] = {}
 with open("merge_tokenizers/version.py", "r") as version_file:
     exec(version_file.read(), VERSION)
 
-with open("requirements.txt", encoding="utf-8") as req_fp:
-    install_requires = req_fp.readlines()
+INSTALL_REQUIRES: List[str] = [
+    "levenshtein",
+    "pydantic",
+    "spacy-alignments",
+    "scikit-learn",
+    "fastdtw",
+    "ukkonen",
+    "numba",
+]
 
-EXTRAS_REQUIRES: Dict[str, List[str]] = {}
-with open("dev-requirements.txt", encoding="utf-8") as dev_req_fp:
-    EXTRAS_REQUIRES["dev"] = dev_req_fp.readlines()
+EXTRAS_REQUIRES: Dict[str, List[str]] = {
+    "dev": [
+        "black",
+        "flake8",
+        "mypy",
+        "types-requests",
+        "pytest",
+        "isort",
+        "autoflake",
+        "pre-commit",
+    ]
+}
 
 extensions = [
     Extension(
@@ -28,7 +44,7 @@ setup(
     author="Symanto Research GmbH",
     author_email="jose.gonzalez@symanto.com",
     packages=find_packages(),
-    install_requires=install_requires,
+    install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRES,
     include_package_data=True,
     ext_modules=extensions,
